d311ae489f32eee427029242c7af5134614b19c2
stelios26 <steliosioakim@gmail.com>
2016-06-05 10:27:48 +0300

    Eddystone is advertising
    Loaded correct version of libraries from manage runtime environment (see
    screenshot), coded advertisements for eddystone and added example url.
    Also flashed proper softdevice 2.0.0.7 which has some changes in LFclk
    handling.

--- main.c
+++ main.c
@@ -3,10 +3,186 @@
 #include <string.h>
 
 #include "app_error.h"
+#include "softdevice_handler.h"
+#include "ble_advdata.h"
 #include "nrf_delay.h"
 #include "nrf_drv_gpiote.h"
+#include "app_timer.h"
 #include "led.h"
 #include "debug.h"
+
+#define APP_TIMER_PRESCALER             0                                 /**< Value of the RTC1 PRESCALER register. */
+#define APP_TIMER_OP_QUEUE_SIZE         4                                 /**< Size of timer operation queues. */
+
+#define CENTRAL_LINK_COUNT              0                                 /**< Number of central links used by the application. When changing this number remember to adjust the RAM settings*/
+#define PERIPHERAL_LINK_COUNT           0                                 /**< Number of peripheral links used by the application. When changing this number remember to adjust the RAM settings*/
+
+#define APP_CFG_NON_CONN_ADV_TIMEOUT    0                                 /**< Time for which the device must be advertising in non-connectable mode (in seconds). 0 disables the time-out. */
+#define NON_CONNECTABLE_ADV_INTERVAL    MSEC_TO_UNITS(100, UNIT_0_625_MS) /**< The advertising interval for non-connectable advertisement (100 ms). This value can vary between 100 ms and 10.24 s). */
+
+#define DEAD_BEEF                       0xDEADBEEF                        /**< Value used as error code on stack dump, can be used to identify stack location on stack unwind. */
+
+// Eddystone common data
+#define APP_EDDYSTONE_UUID              0xFEAA                            /**< UUID for Eddystone beacons according to specification. */
+#define APP_EDDYSTONE_RSSI              0xEE                              /**< 0xEE = -18 dB is the approximate signal strength at 0 m. */
+
+// Eddystone UID data
+#define APP_EDDYSTONE_UID_FRAME_TYPE    0x00                              /**< UID frame type is fixed at 0x00. */
+#define APP_EDDYSTONE_UID_RFU           0x00, 0x00                        /**< Reserved for future use according to specification. */
+#define APP_EDDYSTONE_UID_ID            0x01, 0x02, 0x03, 0x04, \
+                                        0x05, 0x06                        /**< Mock values for 6-byte Eddystone UID ID instance.  */
+#define APP_EDDYSTONE_UID_NAMESPACE     0xAA, 0xAA, 0xBB, 0xBB, \
+                                        0xCC, 0xCC, 0xDD, 0xDD, \
+                                        0xEE, 0xEE                        /**< Mock values for 10-byte Eddystone UID ID namespace. */
+
+// Eddystone URL data
+#define APP_EDDYSTONE_URL_FRAME_TYPE    0x10                              /**< URL Frame type is fixed at 0x10. */
+#define APP_EDDYSTONE_URL_SCHEME        0x00                              /**< 0x00 = "http://www" URL prefix scheme according to specification. */
+#define APP_EDDYSTONE_URL_URL           0x6e, 0x6f, 0x72, 0x64, \
+                                        0x69, 0x63, 0x73, 0x65, \
+                                        0x6d,0x69, 0x00                   /**< "nordicsemi.com". Last byte suffix 0x00 = ".com" according to specification. */
+
+static ble_gap_adv_params_t m_adv_params;                                 /**< Parameters to be passed to the stack when starting advertising. */
+
+static uint8_t eddystone_url_data[] =   /**< Information advertised by the Eddystone URL frame type. */
+{
+    APP_EDDYSTONE_URL_FRAME_TYPE,   // Eddystone URL frame type.
+    APP_EDDYSTONE_RSSI,             // RSSI value at 0 m.
+    APP_EDDYSTONE_URL_SCHEME,       // Scheme or prefix for URL ("http", "http://www", etc.)
+    APP_EDDYSTONE_URL_URL           // URL with a maximum length of 17 bytes. Last byte is suffix (".com", ".org", etc.)
+};
+
+/**@brief Callback function for asserts in the SoftDevice.
+ *
+ * @details This function will be called in case of an assert in the SoftDevice.
+ *
+ * @warning This handler is an example only and does not fit a final product. You need to analyze
+ *          how your product is supposed to react in case of Assert.
+ * @warning On assert from the SoftDevice, the system can only recover on reset.
+ *
+ * @param[in]   line_num   Line number of the failing ASSERT call.
+ * @param[in]   file_name  File name of the failing ASSERT call.
+ */
+void assert_nrf_callback(uint16_t line_num, const uint8_t * p_file_name)
+{
+		debug_printf("Assert from softdevice. failed miserably. resetting... \r\n");
+    app_error_handler(DEAD_BEEF, line_num, p_file_name);
+}
+
+/**@brief Function for initializing the advertising functionality.
+ *
+ * @details Encodes the required advertising data and passes it to the stack.
+ *          Also builds a structure to be passed to the stack when starting advertising.
+ */
+static void advertising_init(void)
+{
+    uint32_t      err_code;
+    ble_advdata_t advdata;
+    uint8_t       flags = BLE_GAP_ADV_FLAGS_LE_ONLY_GENERAL_DISC_MODE;
+    ble_uuid_t    adv_uuids[] = {{APP_EDDYSTONE_UUID, BLE_UUID_TYPE_BLE}};
+
+    uint8_array_t eddystone_data_array;                             // Array for Service Data structure.
+/** @snippet [Eddystone data array] */
+    eddystone_data_array.p_data = (uint8_t *) eddystone_url_data;   // Pointer to the data to advertise.
+    eddystone_data_array.size = sizeof(eddystone_url_data);         // Size of the data to advertise.
+/** @snippet [Eddystone data array] */
+
+    ble_advdata_service_data_t service_data;                        // Structure to hold Service Data.
+    service_data.service_uuid = APP_EDDYSTONE_UUID;                 // Eddystone UUID to allow discoverability on iOS devices.
+    service_data.data = eddystone_data_array;                       // Array for service advertisement data.
+
+    // Build and set advertising data.
+    memset(&advdata, 0, sizeof(advdata));
+
+    advdata.name_type               = BLE_ADVDATA_NO_NAME;
+    advdata.flags                   = flags;
+    advdata.uuids_complete.uuid_cnt = sizeof(adv_uuids) / sizeof(adv_uuids[0]);
+    advdata.uuids_complete.p_uuids  = adv_uuids;
+    advdata.p_service_data_array    = &service_data;                // Pointer to Service Data structure.
+    advdata.service_data_count      = 1;
+
+    err_code = ble_advdata_set(&advdata, NULL);
+    if (err_code == NRF_SUCCESS)
+			debug_printf("Advertising data set!\r\n");
+		else
+		{
+			debug_printf("Ooops.. Something is wrong with setting advertising data..\r\n");
+			APP_ERROR_CHECK(err_code);
+		}
+
+    // Initialize advertising parameters (used when starting advertising).
+    memset(&m_adv_params, 0, sizeof(m_adv_params));
+		
+		debug_printf("Initialising all advertising parameters...\r\n");
+    m_adv_params.type        = BLE_GAP_ADV_TYPE_ADV_NONCONN_IND;
+    m_adv_params.p_peer_addr = NULL;                                // Undirected advertisement.
+    m_adv_params.fp          = BLE_GAP_ADV_FP_ANY;
+    m_adv_params.interval    = NON_CONNECTABLE_ADV_INTERVAL;
+    m_adv_params.timeout     = APP_CFG_NON_CONN_ADV_TIMEOUT;
+}
+
+/**@brief Function for starting advertising.
+ */
+static void advertising_start(void)
+{
+    uint32_t err_code;
+
+    err_code = sd_ble_gap_adv_start(&m_adv_params);
+		if (err_code == NRF_SUCCESS)	
+			debug_printf("Advertisements started!!\r\n");
+		else
+		{
+			debug_printf("Ooops.. Something is wrong..\r\n");
+			APP_ERROR_CHECK(err_code);
+		}
+
+}
+
+/**@brief Function for initializing the BLE stack.
+ *
+ * @details Initializes the SoftDevice and the BLE event interrupt.
+ */
+static void ble_stack_init(void)
+{
+    uint32_t err_code;
+        
+		// Initialize the SoftDevice handler module.
+		SOFTDEVICE_HANDLER_INIT(NRF_CLOCK_LFCLKSRC_XTAL_20_PPM, NULL);
+    debug_printf("Softdeviced initialised.. \r\n");
+	
+    ble_enable_params_t ble_enable_params;
+    err_code = softdevice_enable_get_default_config(CENTRAL_LINK_COUNT,
+                                                    PERIPHERAL_LINK_COUNT,
+                                                    &ble_enable_params);
+		if (err_code == NRF_SUCCESS)
+			debug_printf("Softdevice configured with links and ble parameters!\r\n");
+		else
+		{
+			debug_printf("Ooops.. Something is wrong with setting softdevice parameters..\r\n");
+			APP_ERROR_CHECK(err_code);
+		}
+    
+    //Check the ram settings against the used number of links
+    CHECK_RAM_START_ADDR(CENTRAL_LINK_COUNT,PERIPHERAL_LINK_COUNT);
+    
+    // Enable BLE stack.
+    err_code = softdevice_enable(&ble_enable_params);
+    if (err_code == NRF_SUCCESS)
+			debug_printf("Softdevice enabled successfully!\r\n");
+		else
+		{
+			debug_printf("Ooops.. Something is wrong with enabling softdevice..\r\n");
+			APP_ERROR_CHECK(err_code);
+		}
+}
+
+/**@brief Function for doing power management.
+ */
+static void power_manage(void)
+{
+    uint32_t err_code = sd_app_evt_wait();
+    APP_ERROR_CHECK(err_code);
+}
 
 int main(void)
 {
@@ -20,11 +196,24 @@ int main(void)
 	err_code = nrf_drv_gpiote_init();
 	if (err_code == NRF_SUCCESS)
 		debug_printf("GPIOTE initialised!\r\n");
-	APP_ERROR_CHECK(err_code);
+	else
+	{
+			debug_printf("Ooops.. Something is wrong..\r\n");
+			APP_ERROR_CHECK(err_code);
+	}
 
 	led_init();
 	debug_printf("Visual feedback should be active!\r\n");
 	
+	/* Initialise the RTC1 timer */
+	APP_TIMER_INIT(APP_TIMER_PRESCALER, APP_TIMER_OP_QUEUE_SIZE, false);
+	debug_printf("App timer initialised.. \r\n");
+	
+	ble_stack_init();
+	advertising_init();
+  advertising_start();
+	
+	led_rgb_set(LED_RED, true);
 	led_rgb_set(LED_GREEN, false);
 	led_rgb_set(LED_BLUE, false);
 	
@@ -32,13 +221,6 @@ int main(void)
 	
   while (true)
   {
-		led_rgb_set(LED_RED, true);
-		
-    nrf_delay_ms(500);
-		
-		led_rgb_set(LED_RED, false);
-		
-		nrf_delay_ms(500);
-		debug_printf(".");
+		power_manage();
 	}
 }
---
+++ main.h
@@ -0,0 +1,14 @@
+/*  CCH Beacon
+ *  Copyright (c) 2016 Stelios Ioakim.
+ *  All Rights Reserved.
+ *
+ *  Developed by s.ioakim@engino.net
+ */
+
+#ifndef __main_h
+#define __main_h
+
+
+
+/** @} */
+#endif
